0fd30148f3a20f15e348f89fd15048539b73e332
Hao Lieu <hao.lieu@bmbsoft.com.vn>
2024-06-05 23:36:50 +0700

    dev: food api

--- index.js
+++ index.js
@@ -9,16 +9,44 @@ mongoose.connect("mongodb://127.0.0.1:27017/Foodstall")
   })
   .catch(error => console.log(error))
 
+app.use(express.urlencoded({extended: true}));
 
 // Define the GET API
 app.get('/foods', async (req, res) => {
     try {
-       const food = await Food.find();
-       res.json(food);
+       const foods = await Food.find();
+       res.json(foods);
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
 });
+
+app.get('/foods/:id', async (req, res) => {
+  try {
+    const {id} = req.params;
+    const food = await Food.findById({_id: id});
+    res.json(food);
+  } catch (err) {
+      res.status(500).json({ message: err.message });
+  }
+});
+
+app.post('/foods/create-food', async (req, res) => {
+  const newFood = await new Food(req.body).save();
+  res.send(newFood)
+});
+
+app.put('/foods/:id', async (req, res) => {
+  const {id} = req.params;
+  const food = await Food.findByIdAndUpdate(id, req.body, {runValidators: true, new: true});
+  res.json(food)
+})
+
+app.delete('/foods/:id', async (req, res) => {
+  const {id} = req.params;
+  const deletedFood = await Food.findByIdAndDelete(id, {new: true});
+  res.json(deletedFood)
+})
 
 app.listen(8080, () => {
   console.log("listening on port 8080");
