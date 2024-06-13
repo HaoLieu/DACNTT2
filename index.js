2a40affdd43c86258b374b5b1987c7a3666da191
Hao Lieu <hao.lieu@bmbsoft.com.vn>
2024-06-13 21:06:05 +0700

    upload api

--- index.js
+++ index.js
@@ -560,8 +560,8 @@ const options = {
     },
     servers: [
       {
-          url: 'http://localhost:8080/'
-          // url: 'https://dacntt2.onrender.com/'
+          // url: 'http://localhost:8080/'
+          url: 'https://dacntt2.onrender.com/'
       }
     ]
   },
